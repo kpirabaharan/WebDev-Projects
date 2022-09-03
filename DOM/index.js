f7ec268dcb7c279af78ced8748669124dde55f81
kpirabaharan <spkeeshi@gmail.com>
2022-09-03 10:41:23 -0400

    DOM and jQuery Start

---
+++ DOM/index.js
@@ -0,0 +1,5 @@
+// alert("Hello");
+// document.querySelector("h1").innerHTML = "Good Bye";
+
+
+var heading = document.secondElementChild.firstElementChild.lastElementChild;
---
+++ jQuery/index.js
@@ -0,0 +1,6 @@
+
+// If jQuery library is ready, run js, if jquery and js is linked at top, not necessary
+$(document).ready(function(){
+    
+    $("h1").css("color", "red");
+})
