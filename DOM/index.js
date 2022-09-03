9dc9d9b7d556f088731a584abb8599563bd9a36a
kpirabaharan <kpirabaharan3@gmail.com>
2022-09-03 13:08:10 -0400

    DOM Complete

--- DOM/index.js
+++ DOM/index.js
@@ -1,5 +1,31 @@
 // alert("Hello");
 // document.querySelector("h1").innerHTML = "Good Bye";
 
+//var heading = document.secondElementChild.firstElementChild.lastElementChild;
 
-var heading = document.secondElementChild.firstElementChild.lastElementChild;
+// Changing styles of elements with JS
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").style.fontSize = "5rem";
+document.querySelector("h1").style.padding = "7%";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+// Add/Remove/Toggle a css class to an html element
+// This should be used over changing styles directly as that is good practice
+// HTML = Structure, CSS = Styling, JS = Behaviour
+document.querySelector("button").classList.add("invisible")
+document.querySelector("button").classList.remove("invisible")
+document.querySelector("button").classList.toggle("invisible")
+
+// Add HTML
+console.log(document.querySelector("h1").innerHTML)
+document.querySelector("h1").innerHTML = "<en>Good Bye</en>";
+console.log(document.querySelector("h1").innerHTML)
+
+// document.querySelector("a");
+// <a href=​"https:​/​/​www.google.com">​Google​</a>​
+
+// console.log(document.querySelector("a"));
+// console.log(document.querySelector("a").getAttribute("ref"));
+document.querySelector("a").setAttribute("href", "https://www.apple.com/ca");
+
